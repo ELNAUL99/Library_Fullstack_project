adbc704287114bc4353ad1769309bb10c30ad0eb
ELNAUL99 <kinhnghiemnuoiga99@gmail.com>
2026-04-26 15:51:37 +0300

    polish  database

--- backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c809115f5fbd2deb986ee0bdd2eabf01e4f4f75f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14c3bdedbc8a51108be19d3409d7bba857ecbb10")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
